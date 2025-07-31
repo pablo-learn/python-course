ce436a55d94731841f573e6acca5ed28f9a5b2b1
pabla <pablodelfantexp@gmail.com>
2025-07-31 09:14:42 -0300

    Implement CSV reading and writing functionality; add products.csv and csv_working.py, remove obsolete csv.py and file.csv

--- csv.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-with open('products_updated.csv', 'r') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        print(row)
---
+++ csv_working.py
@@ -0,0 +1,19 @@
+import csv
+
+field_names = ['name', 'price', 'quantity']
+
+#Leer un archivo
+with open('products.csv', mode='r') as file:
+    csv_reader = csv.DictReader(file)
+    
+    with open('products_updated.csv', mode='w') as file:
+        csv_writer = csv.DictWriter(file, fieldnames=field_names)
+        csv_writer.writeheader()
+        for row in csv_reader:
+            csv_writer.writerow(
+                {
+                    'name': row['name'],
+                    'price': row['price'],
+                    'quantity': row['quantity']
+                }
+            )
